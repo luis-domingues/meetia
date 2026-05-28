ea2d9f94f34845f6071297e7bb92e3ac63701ab7
luis-domingues <luisdomingues117@gmail.com>
2026-05-28 15:25:02 -0300

    fix(background): improve message handling and logging

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,10 +1,20 @@
-console.log("background script running");
+import { MessageType } from '../constants/messages';
 
-chrome.runtime.onInstalled.addListener(()=> {
-    console.log("Extension installed");
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Meet.ia extension installed');
 });
 
-chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
-    if(message.type === 'NEW_CAPTION') console.log('New caption captured:', message.data);
+chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+    if(message.type === MessageType.NEW_CAPTION)
+        console.debug('New caption captured:', message.data);
+    
+
+    if(message.type === MessageType.OPEN_POPUP) {
+        chrome.action.openPopup()
+            .then(() => sendResponse({success: true}))
+            .catch(() => sendResponse({success: false}));
+        return true;
+    }
+
     return true;
-})
+});
